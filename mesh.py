0803048b0badde6af9460789c34125589c984de7
Elias Dobrin <elias@dobrin.net>
2022-06-09 20:02:17 -0700

    added uniform triangle mesh

--- mesh.py
+++ mesh.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from math import sqrt
 from vectors import Vector
 
 
@@ -64,6 +65,34 @@ class QuadCrossMesh(Mesh):
                 link = Link(nodes=(nodes_quad[x][y], nodes_quad[x + 1][y + 1]))
                 links.append(link)
                 link = Link(nodes=(nodes_quad[x][y + 1], nodes_quad[x + 1][y]))
+                links.append(link)
+        super().__init__(nodes, links)
+
+
+class TriMesh(Mesh):
+    def __init__(self, grid: tuple[int, int]) -> None:
+        nodes_quad = []
+        for x in range(grid[0] + 1):
+            nodes_quad.append([])
+            for y in range(grid[1] + 1):
+                node = Node(Vector((x + 0.5 * (y % 2)) / grid[0], y * sqrt(3) / (2 * grid[1])))
+                nodes_quad[x].append(node)
+        nodes = [node for nodes_buffer in nodes_quad for node in nodes_buffer]
+        links = []
+        for x in range(grid[0]):
+            for y in range(grid[1] + 1):
+                link = Link(nodes=(nodes_quad[x][y], nodes_quad[x + 1][y]))
+                links.append(link)
+        for x in range(grid[0] + 1):
+            for y in range(grid[1]):
+                link = Link(nodes=(nodes_quad[x][y], nodes_quad[x][y + 1]))
+                links.append(link)
+        for x in range(grid[0]):
+            for y in range(grid[1]):
+                if y % 2 == 0:
+                    link = Link(nodes=(nodes_quad[x][y + 1], nodes_quad[x + 1][y]))
+                else:
+                    link = Link(nodes=(nodes_quad[x][y], nodes_quad[x + 1][y + 1]))
                 links.append(link)
         super().__init__(nodes, links)
 
--- render.py
+++ render.py
@@ -1,17 +1,21 @@
 from math import tau
 import cairo
-from mesh import Mesh, QuadMesh, QuadCrossMesh
+from mesh import Mesh, QuadMesh, QuadCrossMesh, TriMesh
 from vectors import Vector
 
 
-camera_position = Vector(0, 0)
-camera_zoom = 0.3
-
-mesh = QuadCrossMesh((3, 3))
+mesh = TriMesh((4, 4))
 mesh.translate(Vector(-0.5, -0.5))
 
 
 def render(mesh: Mesh) -> None:
+    nodes = mesh.nodes
+    links = mesh.links
+    minimum = Vector(min(node.point.x for node in nodes), min(node.point.y for node in nodes))
+    maximum = Vector(max(node.point.x for node in nodes), max(node.point.y for node in nodes))
+    camera_position = (minimum + maximum) / 2
+    camera_zoom = 0.7 / (max(maximum.x - minimum.x, maximum.y - minimum.y))
+
     surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 1000, 1000)
     context = cairo.Context(surface)
     context.scale(1000, 1000)
@@ -23,14 +27,11 @@ def render(mesh: Mesh) -> None:
     context.scale(camera_zoom, camera_zoom)
     context.translate(-camera_position.x, -camera_position.y)
 
-    nodes = mesh.nodes
-    links = mesh.links
-
     for link in links:
         context.move_to(link.nodes[0].point.x, link.nodes[0].point.y)
         context.line_to(link.nodes[1].point.x, link.nodes[1].point.y)
         context.set_source_rgb(0, 0, 0)
-        context.set_line_width(0.04)
+        context.set_line_width(0.02 / camera_zoom)
         context.set_line_cap(cairo.LINE_CAP_ROUND)
         context.stroke()
 
@@ -39,11 +40,8 @@ def render(mesh: Mesh) -> None:
         context.set_source_rgb(1, 1, 1)
         context.fill_preserve()
         context.set_source_rgb(0, 0, 0)
-        context.set_line_width(0.02)
+        context.set_line_width(0.01 / camera_zoom)
         context.stroke()
-        context.arc(node.point.x, node.point.y, 0.025, 0, tau)
-        context.set_source_rgb(0, 0, 0)
-        context.fill()
 
     surface.write_to_png("render.png")
 
